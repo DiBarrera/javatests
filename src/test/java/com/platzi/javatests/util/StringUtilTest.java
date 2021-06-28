dfd6cac11fcaaa7b845d3e29231b35707fb3da05
Diego Barrera Jiménez <di_barrera@hotmail.com>
2021-06-28 18:56:24 -0500

    Initial Commit

---
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -0,0 +1,11 @@
+package com.platzi.javatests.util;
+
+public class StringUtilTest {
+
+    public static void main(String[] args) {
+
+        String result = StringUtil.repeat("Hola Mundo ", 3);
+
+        System.out.println(result);
+    }
+}
