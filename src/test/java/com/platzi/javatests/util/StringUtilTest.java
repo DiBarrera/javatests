87f97ff1ac9a90f347c8f9c519d4b7ba79a5fd50
Diego Barrera Jiménez <di_barrera@hotmail.com>
2021-06-28 19:06:48 -0500

    Maven Project witn Unitary tests

--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -5,7 +5,21 @@ public class StringUtilTest {
     public static void main(String[] args) {
 
         String result = StringUtil.repeat("Hola Mundo ", 3);
+        // System.out.println(result);
+        if (result.equals("Hola Mundo Hola Mundo Hola Mundo ")) {
+            System.out.println("Ok");
+        }
+        if (!result.equals("Hola Mundo Hola Mundo Hola Mundo ")) {
+            System.out.println("ERROR");
+        }
 
-        System.out.println(result);
+        String result1 = StringUtil.repeat("Hola Mundo Java ", 1);
+        // System.out.println(result1);
+        if (result1.equals("Hola Mundo Java ")) {
+            System.out.println("Ok Java");
+        }
+        if (!result1.equals("Hola Mundo Java ")) {
+            System.out.println("ERROR");
+        }
     }
 }
