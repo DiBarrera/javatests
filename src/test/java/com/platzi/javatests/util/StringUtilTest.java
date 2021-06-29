95026addabc1fae8f7385b8ec5e9605e1083d577
Diego Barrera Jiménez <di_barrera@hotmail.com>
2021-06-28 21:05:44 -0500

    Unitary test with JUnit

--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -1,38 +1,22 @@
 package com.platzi.javatests.util;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringUtilTest {
 
-    public static void main(String[] args) {
+    @Test
+    public void testRepeat() {
 
-        assertEquals(StringUtil.repeat("Hola Mundo ", 3), "Hola Mundo Hola Mundo Hola Mundo ");
-
-        // System.out.println(result);
-        // if (result.equals("Hola Mundo Hola Mundo Hola Mundo ")) {
-        //      System.out.println("Ok");
-        // }
-        // if (!result.equals("Hola Mundo Hola Mundo Hola Mundo ")) {
-        //      System.out.println("ERROR");
-        // }
-
-        assertEquals(StringUtil.repeat("Hola Mundo Java ", 1), "Hola Mundo Java ");
-
-        // if (!result1.equals("Hola Mundo Java ")) {
-        //     throw new RuntimeException("ERROR");
-        // }
-
-        // System.out.println(result1);
-        // if (result1.equals("Hola Mundo Java ")) {
-        //      System.out.println("Ok Java");
-        // }
-        // if (!result1.equals("Hola Mundo Java ")) {
-        //      System.out.println("ERROR JAVA");
-        // }
+        Assert.assertEquals("Hola Mundo Hola Mundo Hola Mundo ", StringUtil.repeat("Hola Mundo ", 3));
+        Assert.assertEquals("Hola Mundo Java ", StringUtil.repeat("Hola Mundo Java ", 1));
     }
-
+/*
     private static void assertEquals(String actual, String expected) {
 
         if (!actual.equals(expected)) {
             throw new RuntimeException(actual + " is not equal to expected " + expected);
         }
     }
+ */
 }
